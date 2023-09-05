55eca4c017f9f89a1afabb71117e9fbdebfc1069
wise-tour <lobatskiy31@gmail.com>
2023-09-05 17:11:31 +0300

    update config

--- src/wigetch/PersonalizationSetting/PersonalizationSetting.tsx
+++ src/wigetch/PersonalizationSetting/PersonalizationSetting.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { UploadFile } from '../../shared/UI/UploadFile/UploadFile';
 import axios from 'axios';
 import s from './PersonalizationSetting.module.scss';
-import { Outlet, useNavigate } from 'react-router';
+import { Outlet, useNavigate, useParams } from 'react-router';
 import { BtnBack } from '../../shared/UI/BtnBack/BtnBack';
 import { BackIcon } from '../../shared/assets/svg/BackIcon';
 import { LineParam } from '../../shared/UI/LineParam/LineParam';
 import { AITextIcon } from '../../shared/assets/svg/AITextIcon';
 import { GraphicIcon } from '../../shared/assets/svg/GraphicIcon';
 import { PersonalizationTypeAria } from '../../shared/UI/Control/PersonalizationTypeAria/PersonalizationTypeAria';
+import { URLS, URL_PAGE } from '../../shared/providers/router/AppRouter';
 export const PersonalizationSetting = () => {
+  const { configID } = useParams();
+  if (!configID) return <></>;
+
   const navigate = useNavigate();
 
   return (
@@ -18,14 +22,13 @@ export const PersonalizationSetting = () => {
         <BtnBack
           icon={<BackIcon />}
           name={'Close personalization'}
-          onClick={() => navigate(-1)}
+          onClick={() => navigate(URL_PAGE.personalizePage(configID))}
         />
       </div>
       <div className={s.main}>
         <PersonalizationTypeAria />
 
         <Outlet />
-        {/* */}
       </div>
       <footer>
         <div className="reset"></div>
